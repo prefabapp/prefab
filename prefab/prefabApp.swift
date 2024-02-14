7d13f285147da7bbaadeb93c8abf7d781ba918ac
kellyp <kellyplummer@mac.com>
2024-02-14 15:11:46 -0800

    Initial Commit

---
+++ prefab/prefabApp.swift
@@ -0,0 +1,17 @@
+//
+//  prefabApp.swift
+//  prefab
+//
+//  Created by Kelly Plummer on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct prefabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
